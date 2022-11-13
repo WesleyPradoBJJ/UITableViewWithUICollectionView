277361c631fdf956151d9be8d126baa52125f746
Wesley Prado <wesleypradobjj@gmail.com>
2022-11-13 18:03:10 +0400

    Creating Project

---
+++ TableViewWithCollectionViewApp/TableViewWithCollectionViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewWithCollectionViewApp
+//
+//  Created by Wesley Prado on 13/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
